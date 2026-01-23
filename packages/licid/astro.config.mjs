135858b85b34968451afbd7e61b68c7376ac7b92
szorfein <szorfein@protonmail.com>
2026-01-23 12:41:36 +0000

    add fonts

--- packages/licid/astro.config.mjs
+++ packages/licid/astro.config.mjs
@@ -4,35 +4,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    fonts: [
-      {
-        provider: "local",
-        name: "Plain",
-        cssVariable: "--font-plain",
-        variants: [
-          {
-            weight: 500,
-            style: "normal",
-            src: ["./src/assets/fonts/IosevkaEtoile-Medium.woff2"],
-            display: "swap",
-          },
-        ],
-      },
-      {
-        provider: "local",
-        name: "Brand",
-        cssVariable: "--font-brand",
-        variants: [
-          {
-            weight: 400,
-            style: "normal",
-            src: ["./src/assets/fonts/IosevkaEtoile-Regular.woff2"],
-            display: "swap",
-          },
-        ],
-      },
-    ],
-  },
   vite: { plugins: [tailwindcss()] },
 });
