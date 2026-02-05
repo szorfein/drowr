478c0534007c2a910d9fa3e0cf7b3726012705a6
szorfein <szorfein@protonmail.com>
2026-02-05 21:14:36 +0000

    add the about page

--- packages/licid/index.ts
+++ packages/licid/index.ts
@@ -4,6 +4,7 @@
 import Header from "./src/Header.astro";
 import HeroHome from "./src/blocks/hero/Home.astro";
 import HeroPost from "./src/blocks/hero/Post.astro";
+import HeroAbout from "./src/blocks/hero/About.astro";
 import HeroBlogPost from "./src/blocks/hero/BlogPost.astro";
 import PreloadFontsHead from "./src/PreloadFontsHead.astro";
 import CardOutlined from "./src/Card/Outlined.astro";
@@ -12,6 +13,7 @@ import ChipsSuggestion from "./src/components/chips/Suggestion.astro";
 import ChipsFilter from "./src/components/chips/Filter.astro";
 import MarkdownCode from "./src/blocks/markdown/code.astro";
 import ShareIt from "./src/blocks/ShareIt.astro";
+import Social from "./src/components/Social.astro";
 
 // Don't use 'export default' here
 export {
@@ -24,6 +26,8 @@ export {
   HeroHome,
   HeroPost,
   HeroBlogPost,
+  HeroAbout,
   MarkdownCode,
   ShareIt,
+  Social,
 };
