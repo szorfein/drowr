420c76d9f5b326a64ff52ec549d908f931e1f4bc
szorfein <szorfein@protonmail.com>
2026-02-08 15:05:19 +0000

    comments with Giscus

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -3,10 +3,16 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import config from "./src/config/config.json";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: config.site.base_url,
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
+
